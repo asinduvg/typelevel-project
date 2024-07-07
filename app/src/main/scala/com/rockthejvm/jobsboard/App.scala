eb3a689e2eefa8f175972dbdaf1e64fbcfdfed87
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2024-07-07 09:44:03 +0530

    Foundations: Tyrian

--- app/src/main/scala/com/rockthejvm/jobsboard/App.scala
+++ app/src/main/scala/com/rockthejvm/jobsboard/App.scala
@@ -1,11 +1,52 @@
 package com.rockthejvm.jobsboard
 
 import scala.scalajs.js.annotation.*
-import org.scalajs.dom.document
+import org.scalajs.dom.{document, console}
+import tyrian.*
+import tyrian.Html.*
+import tyrian.cmds.Logger
+import cats.effect.IO
+import scala.concurrent.duration.*
+
+object App {
+  sealed trait Msg
+  case class Increment(amount: Int) extends Msg
+  case class Model(count: Int)
+}
 
 @JSExportTopLevel("RockTheJvmApp")
-class App {
-  @JSExport
-  def doSomething(containerId: String) =
-    document.getElementById(containerId).innerHTML = "THIS ROCKS"
+class App extends TyrianApp[App.Msg, App.Model] {
+  //                      ^^message ^^ model=state
+  /*
+    We can send messages by
+        - trigger a command
+        - create a subscription
+        - listening for an event
+   */
+  import App.*
+
+  override def init(flags: Map[String, String]): (Model, Cmd[IO, Msg]) =
+    (Model(0), Cmd.None)
+
+  // view triggered whenever model changes
+  override def view(model: Model): Html[Msg] =
+    div(
+      button(onClick(Increment(1)))("Increase"),
+      button(onClick(Increment(-1)))("Decrease"),
+      div(s"Tyrian running: ${model.count}")
+    )
+
+  // potentially endless stream of messages
+  override def subscriptions(model: Model): Sub[IO, Msg] =
+    // Sub.None
+    Sub.every[IO](1.second).map(_ => Increment(1))
+
+    // model can change by receiving messages
+    // model => message => (new model, _____)
+    // update triggered whenever we get a new message
+  override def update(model: Model): Msg => (Model, Cmd[IO, Msg]) = { case Increment(amount) =>
+    // console.log(s"Changing count by $amount")
+    (model.copy(count = model.count + amount), Logger.consoleLog[IO](s"Changing count by $amount"))
+  }
+
 }
