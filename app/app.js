c1bc736a516fe66a624ff7c66250026a082ff6d0
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2024-07-07 11:37:50 +0530

    Single-Page Application (SPA)

--- app/app.js
+++ app/app.js
@@ -1,3 +1,3 @@
 import { RockTheJvmApp } from "./target/scala-3.2.1/app-fastopt";
 
-RockTheJvmApp().launch("app")
+RockTheJvmApp().launch("app");
--- app/src/main/scala/com/rockthejvm/jobsboard/App.scala
+++ app/src/main/scala/com/rockthejvm/jobsboard/App.scala
@@ -1,17 +1,18 @@
 package com.rockthejvm.jobsboard
 
 import scala.scalajs.js.annotation.*
-import org.scalajs.dom.{document, console}
+import org.scalajs.dom.{window}
 import tyrian.*
 import tyrian.Html.*
 import tyrian.cmds.Logger
 import cats.effect.IO
 import scala.concurrent.duration.*
 
+import core.*
+
 object App {
-  sealed trait Msg
-  case class Increment(amount: Int) extends Msg
-  case class Model(count: Int)
+  type Msg = Router.Msg
+  case class Model(router: Router)
 }
 
 @JSExportTopLevel("RockTheJvmApp")
@@ -26,27 +27,46 @@ class App extends TyrianApp[App.Msg, App.Model] {
   import App.*
 
   override def init(flags: Map[String, String]): (Model, Cmd[IO, Msg]) =
-    (Model(0), Cmd.None)
+    val (router, cmd) = Router.startAt(window.location.pathname)
+    (Model(router), cmd)
 
   // view triggered whenever model changes
   override def view(model: Model): Html[Msg] =
     div(
-      button(onClick(Increment(1)))("Increase"),
-      button(onClick(Increment(-1)))("Decrease"),
-      div(s"Tyrian running: ${model.count}")
+      renderNavLink("Jobs", "/jobs"),
+      renderNavLink("Login", "/login"),
+      renderNavLink("Signup", "/signup"),
+      div(s"You are now at: ${model.router.location}")
     )
 
   // potentially endless stream of messages
   override def subscriptions(model: Model): Sub[IO, Msg] =
-    // Sub.None
-    Sub.every[IO](1.second).map(_ => Increment(1))
+    Sub.make( // listener for browser history changes
+      "urlChange",
+      model.router.history.state.discrete
+        .map(_.get)
+        .map(newLocation => Router.ChangeLocation(newLocation, true))
+    )
 
     // model can change by receiving messages
     // model => message => (new model, _____)
     // update triggered whenever we get a new message
-  override def update(model: Model): Msg => (Model, Cmd[IO, Msg]) = { case Increment(amount) =>
-    // console.log(s"Changing count by $amount")
-    (model.copy(count = model.count + amount), Logger.consoleLog[IO](s"Changing count by $amount"))
+  override def update(model: Model): Msg => (Model, Cmd[IO, Msg]) = { case msg: Router.Msg =>
+    val (newRouter, cmd) = model.router.update(msg)
+    (model.copy(router = newRouter), cmd)
   }
+
+  private def renderNavLink(text: String, location: String) =
+    a(
+      href    := location,
+      `class` := "nav-link",
+      onEvent(
+        "click",
+        e => {
+          e.preventDefault()
+          Router.ChangeLocation(location)
+        }
+      )
+    )(text)
 
 }
