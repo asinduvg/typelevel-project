f83f1acbf49fd0ce8102d87ca19c6879e65934b3
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2025-04-10 09:10:46 +0530

    Showing the Job List

--- app/src/main/scala/com/rockthejvm/jobsboard/common/Constants.scala
+++ app/src/main/scala/com/rockthejvm/jobsboard/common/Constants.scala
@@ -14,6 +14,7 @@ object Constants {
     val resetPassword  = s"$root/api/auth/recover"
     val changePassword = s"$root/api/auth/users/password"
     val postJob        = s"$root/api/jobs/create"
+    val jobs           = s"$root/api/jobs"
   }
 
   object cookies {
@@ -21,5 +22,7 @@ object Constants {
     val email    = "email"
     val token    = "token"
   }
+
+  val defaultPageSize = 20
 
 }
